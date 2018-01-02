855ee9b8d0523a512ae267ab1e95aaec4ff83718
yanghuan <1354717176@qq.com>
2018-01-02 20:56:53 +0800

    增加动态链接

--- app/desk/detail/controller/about.php
+++ app/desk/detail/controller/about.php
@@ -1,12 +1,22 @@
 <?php
+
 namespace app\desk\detail\controller;
 
 use app\api\common\logic\Deskbase;
+use app\api\console\article\model\Article AS modelArticle;
+use app\api\console\article\model\ArticleData AS modelArticleData;
 
 class About extends Deskbase
 {
     public function index()
     {
+        $catId = $this->request->param('cat_id', 0);
+        $article = modelArticle::where('catid', $catId)->find();
+        if (!is_null($article)) {
+            $content = modelArticleData::where('id', $article['id'])->value('content');
+            $article['content'] = $content;
+        }
+        $this->assign('article', $article);
         return $this->fetch();
     }
 }
